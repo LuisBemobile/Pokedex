3ba6436d9a022e7d93f3487f2da68ae7f3cda5e5
LuisBemobile <luis@bemobile.tech>
2023-11-14 16:45:55 -0300

    feat(pokemon): feat pokemon component that renders one pokemon at a time

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const Pokemon = ({ pokemon }) => {
+  const { name, type, averageWeight, image } = pokemon
+
+  return (
+    <div className="pokemon">
+      <div>
+        <p>{name}</p>
+        <p>{type}</p>
+        <p>
+          Average weight:
+          <span>
+            {`${averageWeight.value} ${averageWeight.measurementUnit}`}
+          </span>
+        </p>
+      </div>
+      <img src={image} alt={`${name}`} />
+    </div>
+  );
+};
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number.isRequired,
+      measurementUnit: PropTypes.string.isRequired,
+    }).isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Pokemon;
